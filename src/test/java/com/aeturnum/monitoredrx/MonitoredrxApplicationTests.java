53b55bff5189feda54ba5abf398a78b5af7a972f
MalindaPieris <MalindaPieris@Malinda-Pieris>
2022-05-27 08:24:02 +0530

    initial commit

---
+++ src/test/java/com/aeturnum/monitoredrx/MonitoredrxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeturnum.monitoredrx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoredrxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
