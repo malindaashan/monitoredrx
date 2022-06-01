a5f25b80f842dda9174f2a0b8c0418c2d5c1846c
MalindaPieris <MalindaPieris@Malinda-Pieris>
2022-06-01 10:10:26 +0530

    adding build files from react. readme.md changed

--- src/main/resources/sqlfile.sql
+++ src/main/resources/sqlfile.sql
@@ -1,3 +1,7 @@
+--create a database as monitoredrx (configurable in application.properties)
+--create a instance as rx in postgres.
+--configure database password in application.properties
+
 create table patient(
 	id serial PRIMARY KEY,
 	firstname varchar(255),
@@ -9,9 +13,3 @@ create table patient(
 	phonenumber varchar(50),
 	email varchar(100)
 );
-
-INSERT INTO public.patient(
-	firstname, lastname, address, city, state, zipcode, phonenumber, email)
-	VALUES ('Malinda', 'Peiris', '28/5, Weliveriya', 'Gampaha', 'Western', 11710, '0717083607', 'malinda.ashan@gmail.com');
-
-select * from patient;
